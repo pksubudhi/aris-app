9253a2236565241eac083a27344cdaa0a37746bc
NimaFeizi <nima_is_here@hotmail.com>
2020-05-26 17:14:43 -0400

    UI UX Branch ARIS Mar 25

--- lib/Home_AppBar_Pages/Profile/profile.dart
+++ lib/Home_AppBar_Pages/Profile/profile.dart
@@ -10,119 +10,148 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
+    double heightScale = Styles.displayHeight(context) / 896;
+
     return Scaffold(
         backgroundColor: Styles.pageBackground,
         appBar: HomeAppBar(
           title: Text('Profile', style: Styles.title),
         ),
-        body: SingleChildScrollView(
+        body: SizedBox(
+            height: Styles.displayHeight(context),
             child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: Styles.displayHeight(context)*0.02),
-            _profileName(context),
-            SizedBox(height:Styles.displayHeight(context)*0.05),
-            _profileNavTab(context),
-            SizedBox(height: Styles.displayHeight(context)*0.037),
-            _profileWorkout(context),
-            SizedBox(height: Styles.displayHeight(context)*0.025),
-            _profileRehab(context),
-            SizedBox(height: Styles.displayHeight(context)*0.025),
-            _profileSport(context),
-          ],
-        )));
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: heightScale * 24),
+                _profileName(context),
+                SizedBox(height: heightScale * 45),
+                _profileNavTab(context),
+                SizedBox(height: heightScale * 33),
+                _profileWorkout(context),
+                SizedBox(height: heightScale * 23),
+                _profileRehab(context),
+                SizedBox(height: heightScale * 23),
+                _profileSport(context),
+              ],
+            )));
   }
 
   Widget _profileName(BuildContext context) {
+    double sizeScale = Styles.screenSize(context) / 987;
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Icon(Icons.account_circle, color: Styles.homeBlue, size: 80.0),
+        Icon(Icons.account_circle,
+            color: Styles.homeBlue, size: 80.0 * sizeScale),
         Text(
           "Lebron James",
           style: Styles.title,
+          textScaleFactor: sizeScale,
         )
       ],
     );
   }
 
   Widget _profileNavTab(BuildContext context) {
+    double sizeScale = Styles.screenSize(context) / 987;
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        Text("Activity", style: Styles.subTitleUnderlined),
-        Text("Stats", style: Styles.subTitle),
-        Text("Info", style: Styles.subTitle)
+        Text("Activity",
+            style: Styles.subTitleUnderlined, textScaleFactor: sizeScale),
+        Text("Stats", style: Styles.subTitle, textScaleFactor: sizeScale),
+        Text("Info", style: Styles.subTitle, textScaleFactor: sizeScale)
       ],
     );
   }
 
   Widget _profileWorkout(context) {
+    double sizeScale = Styles.screenSize(context) / 987;
+
     return Stack(
       children: [
         _colorBox(Styles.arisGreen, 'Workout', 'March 10'),
         _greyBox(
-            Icon(Icons.directions_run, size: 100.0, color: Styles.textWhite),
-            Icon(Icons.info_outline, size: 40.0, color: Styles.arisGreen),
+            Icon(Icons.directions_run,
+                size: sizeScale * 100.0, color: Styles.textWhite),
+            Icon(Icons.info_outline,
+                size: sizeScale * 40.0, color: Styles.arisGreen),
             'Running'),
       ],
     );
   }
 
   Widget _profileRehab(context) {
+    double sizeScale = Styles.screenSize(context) / 987;
+
     return Stack(
       children: [
         _colorBox(Styles.arisBlue, 'Rehab', 'March 1'),
         _greyBox(
-            Icon(Icons.favorite_border, size: 100.0, color: Styles.textWhite),
-            Icon(Icons.info_outline, size: 40.0, color: Styles.arisBlue),
+            Icon(Icons.favorite_border,
+                size: sizeScale * 100.0, color: Styles.textWhite),
+            Icon(Icons.info_outline,
+                size: sizeScale * 40.0, color: Styles.arisBlue),
             'Recovery'),
       ],
     );
   }
 
   Widget _profileSport(context) {
+    double sizeScale = Styles.screenSize(context) / 987;
+
     return Stack(
       children: [
         _colorBox(Styles.arisBlack, 'Sport', 'March 21'),
         _greyBox(
-            Icon(Icons.tonality, size: 100.0, color: Styles.textWhite),
-            Icon(Icons.info_outline, size: 40.0, color: Styles.arisBlack),
+            Icon(Icons.tonality,
+                size: sizeScale * 100.0, color: Styles.textWhite),
+            Icon(Icons.info_outline,
+                size: sizeScale * 40.0, color: Styles.arisBlack),
             'Basketball'),
       ],
     );
   }
 
   Widget _colorBox(Color color, String label, String date) {
+    double sizeScale = Styles.screenSize(context) / 987;
+
     return Container(
       decoration: BoxDecoration(
           color: color,
           borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30), topRight: Radius.circular(30))),
-      margin: EdgeInsets.fromLTRB(20.0, 0, 20.0, 0),
+              topLeft: Radius.circular(sizeScale * 30),
+              topRight: Radius.circular(sizeScale * 30))),
+      margin: EdgeInsets.fromLTRB(sizeScale * 20.0, 0, sizeScale * 20.0, 0),
       child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            Text(label, style: Styles.homeTitles),
-            Text(date, style: Styles.infoWhiteS),
+            Text(label, style: Styles.homeTitles, textScaleFactor: sizeScale),
+            Text(date, style: Styles.infoWhiteS, textScaleFactor: sizeScale),
           ]),
     );
   }
 
   Widget _greyBox(Icon icon, Icon details, String status) {
+    double sizeScale = Styles.screenSize(context) / 987;
+
     return Container(
       color: Styles.pageBackground,
-      margin: EdgeInsets.fromLTRB(0, 50.0, 0, 0),
+      margin: EdgeInsets.fromLTRB(0, sizeScale * 50.0, 0, 0),
       child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             icon,
             details,
-            Text(status, style: Styles.homeTitles, textAlign: TextAlign.end),
+            Text(status,
+                style: Styles.homeTitles,
+                textAlign: TextAlign.end,
+                textScaleFactor: sizeScale),
           ]),
     );
   }
-
 }
--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
-//import 'package:arisapp/Home_AppBar_Pages/Profile/profile.dart';
+import 'package:arisapp/Home_AppBar_Pages/Profile/profile.dart';
 import 'package:arisapp/home_page.dart';
 import 'package:arisapp/login_page.dart';
-//import 'package:arisapp/login_page.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,6 +8,6 @@ import 'package:flutter/material.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home:LoginPage());
+    return MaterialApp(home:Profile());
   }
 }
