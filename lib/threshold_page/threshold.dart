86ead3f38795b9b3645776b22e7c5b34e5494140
mumli516 <ali@pace-tech.com>
2020-08-16 04:58:51 +0500

    by ali - threshold screen -> Heat Map (partially done)

--- lib/threshold_page/threshold.dart
+++ lib/threshold_page/threshold.dart
@@ -40,58 +40,85 @@ class _ThresholdState extends State<ThresholdPage> {
         appBar: HomeAppBar(
         title: Text('Feedback', style: Styles.title),
     ),
-      body:Column(
-        children: <Widget>[
-          Container(
-            height: screen/15,
-          ),
-          Text(
-            "Left Foot",
-            style: TextStyle(
-              fontFamily: GoogleFonts.roboto.toString(),
-              color:  Color(int.parse("FFFFFF".substring(0, 6), radix: 16) + 0xFF000000),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 250,
+              width: 400,
+              child: Stack(
+                children: <Widget>[
+                  new Positioned(left:25.0, child: Image.asset("assets/feet.png"),),
+                  new Positioned(
+                      child: Container(
+                          margin: const EdgeInsets.only(top: 70.0, right: 90),
+                          child: new charts.ScatterPlotChart(
+                            _createSampleData2(),
+                            animate: false,
+                            primaryMeasureAxis: new charts.NumericAxisSpec(
+                                showAxisLine: false,
+                                renderSpec: new charts.NoneRenderSpec()),
+                            domainAxis: new charts.NumericAxisSpec(
+                              showAxisLine: false,
+                                renderSpec: new charts.NoneRenderSpec()),
+                          )
+                      ),
+                  ),
+                ],
+              ),
             ),
-            textAlign: TextAlign.center,
-            textScaleFactor: screen/500,
-            softWrap: true,
-          ),
-          Container(
-            child: new charts.TimeSeriesChart(
-              seriesList,
-              animate: animate,
-              // This is the default configuration, but is shown here for  illustration.
-              defaultRenderer: new charts.LineRendererConfig(),
-              // Custom renderer configuration for the point series.
-             /* customSeriesRenderers: [
+
+            /*Container(
+              height: screen/15,
+            ),*/
+            Text(
+              "Left Foot",
+              style: TextStyle(
+                fontFamily: GoogleFonts.roboto.toString(),
+                color:  Color(int.parse("FFFFFF".substring(0, 6), radix: 16) + 0xFF000000),
+              ),
+              textAlign: TextAlign.center,
+              textScaleFactor: screen/500,
+              softWrap: true,
+            ),
+            Container(
+              child: new charts.TimeSeriesChart(
+                seriesList,
+                animate: animate,
+                // This is the default configuration, but is shown here for  illustration.
+                defaultRenderer: new charts.LineRendererConfig(),
+                // Custom renderer configuration for the point series.
+                /* customSeriesRenderers: [
                 new charts.PointRendererConfig(
                   // ID used to link series to this renderer.
                     customRendererId: 'customPoint')
               ],*/
-              // Optionally pass in a [DateTimeFactory] used by the chart. The factory
-              // should create the same type of [DateTime] as the data provided. If none
-              // specified, the default creates local date time.
-              dateTimeFactory: const charts.LocalDateTimeFactory(),
-              primaryMeasureAxis: new charts.NumericAxisSpec(
-                  renderSpec: new charts.SmallTickRendererSpec(
-                    // Tick and Label styling here.
-                  )
-              ),
-              behaviors: [
-                new charts.ChartTitle('Temperature (F)',
-                    behaviorPosition: charts.BehaviorPosition.start,
-                    titleOutsideJustification:
-                    charts.OutsideJustification.middleDrawArea),
-                new charts.SeriesLegend(
-                    desiredMaxRows:3,
-                  desiredMaxColumns: 3,
+                // Optionally pass in a [DateTimeFactory] used by the chart. The factory
+                // should create the same type of [DateTime] as the data provided. If none
+                // specified, the default creates local date time.
+                dateTimeFactory: const charts.LocalDateTimeFactory(),
+                primaryMeasureAxis: new charts.NumericAxisSpec(
+                    renderSpec: new charts.SmallTickRendererSpec(
+                      // Tick and Label styling here.
+                    )
                 ),
-              ],
+                behaviors: [
+                  new charts.ChartTitle('Temperature (F)',
+                      behaviorPosition: charts.BehaviorPosition.start,
+                      titleOutsideJustification:
+                      charts.OutsideJustification.middleDrawArea),
+                  new charts.SeriesLegend(
+                    desiredMaxRows:3,
+                    desiredMaxColumns: 3,
+                  ),
+                ],
+              ),
+              height: screen/3,
             ),
-            height: screen/3,
-          ),
 
-        ],
-      ),
+          ],
+        ),
+      )
     );
   }
 
@@ -251,11 +278,132 @@ new charts.Series<TimeSeriesTemperature, DateTime>(
 ];
 }
 
+/// Create one series with sample hard coded data.
+List<charts.Series<HeatLocation, double>> _createSampleData2() {
+  final sensorCandDright = [
+    new HeatLocation(3.4, 10, 23.0),
+  ];
+
+  final sensorAandBleft = [
+    new HeatLocation(1.7, 10, 23.0),
+  ];
+
+  final sensorCandDleft = [
+    new HeatLocation(1.1, 9.0, 15.0),
+  ];
+
+  final sensorEleft = [
+    new HeatLocation(1.45, 6.5, 22.0),
+  ];
+
+  final sensorEright = [
+    new HeatLocation(3.7, 6.5, 22.0),
+  ];
+
+  final sensorAandBright = [
+    new HeatLocation(4, 9.0, 15.0),
+  ];
+
+  final sensorGandFright = [
+    new HeatLocation(3.45, 3.5, 22.0),
+  ];
+
+  final sensorGandFleft = [
+    new HeatLocation(1.65, 3.5, 22.0),
+  ];
+
+
+
+  return [
+    new charts.Series<HeatLocation, double>(
+      id: 'sensorCandDright',
+      domainFn: (HeatLocation heatPoint, _) => heatPoint.x,
+      measureFn: (HeatLocation heatPoint, _) => heatPoint.y,
+      // Providing a radius function is optional.
+      radiusPxFn: (HeatLocation heatPoint, _) => heatPoint.radius,
+      colorFn: (_, __) => charts.MaterialPalette.skin,
+      data: sensorCandDright,
+    ),
+    new charts.Series<HeatLocation, double>(
+      id: 'sensorAandBright',
+      domainFn: (HeatLocation heatPoint, _) => heatPoint.x,
+      measureFn: (HeatLocation heatPoint, _) => heatPoint.y,
+      // Providing a radius function is optional.
+      radiusPxFn: (HeatLocation heatPoint, _) => heatPoint.radius,
+      colorFn: (_, __) => charts.MaterialPalette.skin,
+      data: sensorAandBright,
+    ),
+    new charts.Series<HeatLocation, double>(
+      id: 'sensorAandBleft',
+      domainFn: (HeatLocation heatPoint, _) => heatPoint.x,
+      measureFn: (HeatLocation heatPoint, _) => heatPoint.y,
+      // Providing a radius function is optional.
+      radiusPxFn: (HeatLocation heatPoint, _) => heatPoint.radius,
+      colorFn: (_, __) => charts.MaterialPalette.skin,
+      data: sensorAandBleft,
+    ),
+    new charts.Series<HeatLocation, double>(
+      id: 'sensorCandDleft',
+      domainFn: (HeatLocation heatPoint, _) => heatPoint.x,
+      measureFn: (HeatLocation heatPoint, _) => heatPoint.y,
+      // Providing a radius function is optional.
+      radiusPxFn: (HeatLocation heatPoint, _) => heatPoint.radius,
+      colorFn: (_, __) => charts.MaterialPalette.deepOrange.shadeDefault,
+      data: sensorCandDleft,
+    ),
+    new charts.Series<HeatLocation, double>(
+      id: 'sensorEleft',
+      domainFn: (HeatLocation heatPoint, _) => heatPoint.x,
+      measureFn: (HeatLocation heatPoint, _) => heatPoint.y,
+      // Providing a radius function is optional.
+      radiusPxFn: (HeatLocation heatPoint, _) => heatPoint.radius,
+      colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
+      data: sensorEleft,
+    ),
+    new charts.Series<HeatLocation, double>(
+      id: 'sensorEright',
+      domainFn: (HeatLocation heatPoint, _) => heatPoint.x,
+      measureFn: (HeatLocation heatPoint, _) => heatPoint.y,
+      // Providing a radius function is optional.
+      radiusPxFn: (HeatLocation heatPoint, _) => heatPoint.radius,
+      colorFn: (_, __) => charts.MaterialPalette.green.shadeDefault,
+      data: sensorEright,
+    ),
+    new charts.Series<HeatLocation, double>(
+      id: 'sensorGandFright',
+      domainFn: (HeatLocation heatPoint, _) => heatPoint.x,
+      measureFn: (HeatLocation heatPoint, _) => heatPoint.y,
+      // Providing a radius function is optional.
+      radiusPxFn: (HeatLocation heatPoint, _) => heatPoint.radius,
+      colorFn: (_, __) => charts.MaterialPalette.lightCoral,
+      data: sensorGandFright,
+    ),
+    new charts.Series<HeatLocation, double>(
+      id: 'sensorGandFleft',
+      domainFn: (HeatLocation heatPoint, _) => heatPoint.x,
+      measureFn: (HeatLocation heatPoint, _) => heatPoint.y,
+      // Providing a radius function is optional.
+      radiusPxFn: (HeatLocation heatPoint, _) => heatPoint.radius,
+      colorFn: (_, __) => charts.MaterialPalette.lightCoral,
+      data: sensorGandFleft,
+    ),
+  ];
+}
+
 /// Sample time series data type.
 class TimeSeriesTemperature {
   final DateTime time;
   final int sales;
 
   TimeSeriesTemperature(this.time, this.sales);
+}
+
+/// Sample linear data type.
+class HeatLocation {
+  final double x;
+  final double y;
+  final double radius;
+
+  HeatLocation(this.x, this.y, this.radius);
 }
 
